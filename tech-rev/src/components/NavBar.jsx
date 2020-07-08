89d098226b3282ce4499df3c2bfdf5b26088d239
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-07-08 14:02:06 +0100

    created homepage and nav componaent

---
+++ tech-rev/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+
+const NavBar = (props) => {
+  return (
+    <div>
+      <h1>This is Nav</h1>
+    </div>
+  );
+};
+
+export default NavBar;
