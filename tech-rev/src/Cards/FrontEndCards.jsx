fede7ec7dc990e37fab7619ffdc083b2d4034ede
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-07-09 15:35:01 +0100

    added star rating but still need more styling

--- tech-rev/src/Cards/FrontEndCards.jsx
+++ tech-rev/src/Cards/FrontEndCards.jsx
@@ -1,46 +1,61 @@
-import React from 'react';
+import React from "react";
+import StarRatingComponent from "react-star-rating-component";
 import { CardDeck, Card } from "react-bootstrap";
 import { Link } from "@reach/router";
-
 
 const FrontEndCards = () => {
   return (
     <div>
       <CardDeck>
-        <Card>
+        <Card bg={"dark"}>
           <Card.Img variant="top" />
-          <Card.Body>
+          <Card.Body style={{ color: "#36d1dc" }}>
             <Card.Title>React Bootstrap</Card.Title>
             <Card.Text>
-              React component integrated with Bootstrap styling
-          </Card.Text>
-            <Link style={{ color: 'black' }} to={'/felibraries/reactbootstrap'}>Link</Link>
+              Popular bootstrap designs rebuilt for seamless react integration
+            </Card.Text>
+            <StarRatingComponent
+              name="overAllRating"
+              editing={false}
+              renderStarIcon={() => <span>★</span>}
+              starCount={5}
+              value={4}
+            />
+            <StarRatingComponent
+              name="overAllRating"
+              editing={false}
+              renderStarIcon={() => <span>★</span>}
+              starCount={5}
+              value={4}
+            />
+            <Link style={{ color: "black" }} to={"/felibraries/reactbootstrap"}>
+              Link
+            </Link>
           </Card.Body>
           <Card.Footer>
             <small className="text-muted">Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
-        <Card>
+        <Card bg={"dark"}>
           <Card.Body>
             <Card.Title>Card title</Card.Title>
             <Card.Text>
               This card has supporting text below as a natural lead-in to
-            additional content.{" "}
+              additional content.{" "}
             </Card.Text>
           </Card.Body>
           <Card.Footer>
             <small className="text-muted">Last updated 3 mins ago</small>
           </Card.Footer>
         </Card>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+        <Card bg={"dark"}>
           <Card.Body>
             <Card.Title>Card title</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in
-              to additional content. This card has even longer content than the
-              first to show that equal height action.
-          </Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </Card.Text>
           </Card.Body>
           <Card.Footer>
             <small className="text-muted">Last updated 3 mins ago</small>
