69022fd80aa570a9ad9b05661df3449917b9695e
keirbetts <keirjbetts@gmail.com>
2020-07-13 15:32:18 +0100

    styled homepage

--- tech-rev/src/components/title.jsx
+++ tech-rev/src/components/title.jsx
@@ -1,17 +1,45 @@
 import React from "react";
+import { Button, Container, Row, Col } from 'react-bootstrap'
 
 const Title = () => {
   return (
-    <h1
-      style={{
-        color: "whitesmoke",
-        textAlign: "center",
-        fontFamily: "Courier New",
-      }}
-    ><strong>
-        Tech Review
-      </strong>
-    </h1>
+    <div className='app'>
+      <main>
+        <br></br>
+        <br></br>
+        <Container fluid='true'>
+          <Col style={{ color: 'whitesmoke', fontFamily: 'Courier New' }}>
+            <Row lg={12} className='justify-content-center' style={{ marginTop: '50px' }}>
+              <h1><strong>Tech Review</strong></h1>
+            </Row>
+            <br></br>
+            <Row className='justify-content-center'>
+              <h3 lg={12}>Your One Stop Destination for Library and Package Reviews</h3>
+            </Row>
+          </Col>
+          <br></br>
+          <hr className='hr'></hr>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <Row className='justify-content-center' style={{ marginTop: '-100px', textAlign: 'center' }}>
+            <Col sm={4} lg={2}>
+              <Button style={{ borderRadius: '10px 10px 10px 10px' }} variant="success" href='/felibraries'>Front End Libraries</Button>
+            </Col>
+            <Col sm={4} lg={2}>
+              <Button style={{ borderRadius: '10px 10px 10px 10px', color: 'white' }} variant="info" href='/belibraries' >Back End Libraries</Button>
+            </Col>
+            <Col sm={4} lg={2}>
+              <Button style={{ borderRadius: '10px 10px 10px 10px', color: 'white' }} variant="danger" href='/npm-packages' >NPM Packages</Button>
+            </Col>
+          </Row>
+        </Container>
+        <br></br>
+      </main>
+    </div >
   );
 };
 
