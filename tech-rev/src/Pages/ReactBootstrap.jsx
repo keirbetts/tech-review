ef9aac5f029d455b65a895867827aa507041d403
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-07-14 16:16:04 +0100

    fix page tear

--- tech-rev/src/Pages/ReactBootstrap.jsx
+++ tech-rev/src/Pages/ReactBootstrap.jsx
@@ -7,7 +7,7 @@ import rbimg from "../img/react-bootstrap.png";
 const ReactBootstrap = () => {
   return (
     <div>
-      <Container fluid='true' className='con-top'>
+      <Container fluid="true" className="con-top">
         <h1
           style={{
             color: "whitesmoke",
@@ -21,8 +21,12 @@ const ReactBootstrap = () => {
         <Row>
           <Col sm={1}>
             <br></br>
-            <img alt="" src={rbimg} style={{ height: "auto", width: "20rem", marginTop: '-1rem' }} />
-            <hr className='hr' style={{ width: '20rem' }}></hr>
+            <img
+              alt=""
+              src={rbimg}
+              style={{ height: "auto", width: "20rem", marginTop: "-1rem" }}
+            />
+            <hr className="hr" style={{ width: "20rem" }}></hr>
             <h5 style={{ float: "left", color: "whitesmoke" }}>
               <strong>Overall Rating:</strong>
             </h5>
@@ -77,16 +81,16 @@ const ReactBootstrap = () => {
               <p>
                 Best for: beginners and those looking to quickly create usable
                 components with ease.
-            </p>
-
-              <p>
-                Good: Makes responsive design simple. A range of components which
-              can be effectively used. Produces better looking websites.{" "}
               </p>
 
               <p>
-                Limitations: Designs can be limited, those with better css skills
-              should opt to make use of these in most instances.{" "}
+                Good: Makes responsive design simple. A range of components
+                which can be effectively used. Produces better looking websites.{" "}
+              </p>
+
+              <p>
+                Limitations: Designs can be limited, those with better css
+                skills should opt to make use of these in most instances.{" "}
               </p>
             </div>
           </Col>
@@ -98,14 +102,14 @@ const ReactBootstrap = () => {
             <p>
               React Bootstrap is effective and easy to use but some components
               remained limited as they cannot be styled to a great extent.
-          </p>
+            </p>
             {/* This issue is highlighted by the carousel where styling is difficult and no default styles are available for images etc. */}
             <p>
               Largely dissipates the difficulties caused by CSS, by giving
               significant control over individual components, but certain
               components remain difficult to integrate successfully.
-          </p>
-            <hr className='hr'></hr>
+            </p>
+            <hr className="hr" style={{ width: "25rem" }}></hr>
             <h3>
               <strong>Documentation:</strong>
             </h3>
@@ -113,17 +117,19 @@ const ReactBootstrap = () => {
               Clear with good examples. Advice on how to install and import is
               clear and easy to find. The docs should not pose and difficulties
               even for the inexperienced user.
-          </p>
+            </p>
             <p>
-              In many instances examples can be copied and pasted with only minor
-              adjustments
-          </p>
+              In many instances examples can be copied and pasted with only
+              minor adjustments
+            </p>
             <p>
               {" "}
               The number of components and their flexibility means it can take
-            time to find what you're looking for.{" "}
+              time to find what you're looking for.{" "}
             </p>
-            <hr style={{ borderTop: "2px solid lightblue", width: "auto" }}></hr>
+            <hr
+              style={{ borderTop: "2px solid lightblue", width: "25rem" }}
+            ></hr>
             <h1>
               <strong>Components</strong>
             </h1>
@@ -132,10 +138,10 @@ const ReactBootstrap = () => {
             </h3>
             <p>
               {" "}
-              A responsive container is effective and easy to use. The Col and Row
-              components work well and the props system for responsive design is
-              simple.
-          </p>
+              A responsive container is effective and easy to use. The Col and
+              Row components work well and the props system for responsive
+              design is simple.
+            </p>
             <h3>
               <strong>Carousel</strong>
             </h3>
@@ -143,18 +149,18 @@ const ReactBootstrap = () => {
               {" "}
               Creates a carousel with ease. However, bootstrap does not offer
               styling within the carousel, this means adding images is time
-              consuming and difficult, especially for the inexpienced css user. A
-              controlled carousel is also an option.
-          </p>
+              consuming and difficult, especially for the inexpienced css user.
+              A controlled carousel is also an option.
+            </p>
             <h3>
               <strong>Container</strong>
             </h3>
             <p>
               {" "}
-              A responsive container is effective and easy to use. The Col and Row
-              components work well and the props system for responsive design is
-              simple.
-          </p>
+              A responsive container is effective and easy to use. The Col and
+              Row components work well and the props system for responsive
+              design is simple.
+            </p>
             <h3>
               <strong>Documentation</strong>
             </h3>
@@ -164,10 +170,10 @@ const ReactBootstrap = () => {
               install and import is clear and easy to find. The docs should not
               pose and difficulties even for the inexperienced user. One issue
               that could be raised is the number of components and their
-              flexibility means it can take time to find what you're looking for.
-              In many instances examples can be copied and pasted with only minor
-              adjustments
-          </p>
+              flexibility means it can take time to find what you're looking
+              for. In many instances examples can be copied and pasted with only
+              minor adjustments
+            </p>
           </Col>
         </Row>
       </Container>
