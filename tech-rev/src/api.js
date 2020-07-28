e8fbbba5fbe4d8e18e4df7bd091958eddc041ae1
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-07-28 16:33:52 +0100

    more api calls

--- tech-rev/src/api.js
+++ tech-rev/src/api.js
@@ -1,12 +1,33 @@
 const axios = require("axios");
 
-
 exports.fetchReactBootstrap = () => {
-  return axios.get('https://api.npmjs.org/downloads/point/last-month/react-bootstrap').then(({ data }) => {
-    return data
-  })
-}
+  return axios
+    .get("https://api.npmjs.org/downloads/point/last-month/react-bootstrap")
+    .then(({ data }) => {
+      return data;
+    });
+};
 
+exports.fetchSemanticUi = () => {
+  return axios
+    .get("https://api.npmjs.org/downloads/point/last-month/semantic-ui")
+    .then(({ data }) => {
+      return data;
+    });
+};
 
+exports.fetchReactMdl = () => {
+  return axios
+    .get("https://api.npmjs.org/downloads/point/last-month/react-mdl")
+    .then(({ data }) => {
+      return data;
+    });
+};
 
-
+exports.fetchMaterialUi = () => {
+  return axios
+    .get("https://api.npmjs.org/downloads/point/last-month/material-ui")
+    .then(({ data }) => {
+      return data;
+    });
+};
